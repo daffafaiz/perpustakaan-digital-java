d4354c3733722e53697e37f2cbe883bf82270f97
daffafaiz <daffafaiz510@gmail.com>
2026-06-14 08:39:02 +0700

    Delete main directory

--- main/java/com/perpustakaan/digital/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.perpustakaan.digital.exception;
-
-/** Dilempar ketika aturan bisnis dilanggar (mis. stok habis, masih ada pinjaman aktif). */
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- main/java/com/perpustakaan/digital/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.perpustakaan.digital.exception;
-
-/** Dilempar ketika data yang diminta tidak ditemukan. */
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
